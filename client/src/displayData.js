18b0bac541fb9132af60daf784a006d4cf11fa5c
Awadhoot <awadhootk6@gmail.com>
2023-07-14 16:05:48 +0530

    added search functionality

--- client/src/displayData.js
+++ client/src/displayData.js
@@ -1,5 +1,5 @@
-import { useQuery, gql } from '@apollo/client'
-import React from 'react'
+import { useQuery, useLazyQuery, gql } from '@apollo/client'
+import React, { useState } from 'react'
 import './displayData.css'
 
 const QUERY_ALL_USERS = gql`
@@ -13,8 +13,22 @@ const QUERY_ALL_USERS = gql`
   }
 `
 
+const GetUserByID = gql`
+  query getUserByID($userID: ID!) {
+    user(id: $userID) {
+      username
+      name
+      age
+      nationality
+    }
+  }
+`
+
 function Data () {
   const { data, loading, error } = useQuery(QUERY_ALL_USERS)
+  const [username, setUsername] = useState('')
+  const [fetchUser, { data: userData, error: userError }] =
+    useLazyQuery(GetUserByID)
 
   if (error) {
     return <h1>An error occurred!</h1>
@@ -28,21 +42,49 @@ function Data () {
     console.log(data)
   }
 
+  if (userData) {
+    console.log('FOUND!!!')
+  }
+
   return (
-    <div className='container'>
-      {data &&
-        data['users'].map(user => (
-          <div key={user.id}>
-            <ul>
-              <li className='name'>Name: {user.name}</li>
-              <li>Username: {user.username}</li>
-              <li>Country: {user.nationality.toLowerCase()}</li>
-              <li className='friends'>Friends: {user.friends}</li>
-            </ul>
-            <hr />
+    <>
+      <div className='search-box'>
+        <input
+          type='text'
+          placeholder='Enter Username'
+          onChange={e => setUsername(e.target.value)}
+        />
+        <button onClick={() => fetchUser({ variables: { userID: username } })}>
+          Search
+        </button>
+      </div>
+      {userData && !userError ? (
+        <div>
+          <h4>User Found</h4>
+          <div className='container my-div'>
+            <li>Name: {userData.user.name}</li>
+            <li>Age: {userData.user.age}</li>
+            <li>Nationality: {userData.user.nationality}</li>
           </div>
-        ))}
-    </div>
+        </div>
+      ) : (
+        <h4>User not found!</h4>
+      )}
+      <div className='container'>
+        {data &&
+          data['users'].map(user => (
+            <div key={user.id}>
+              <ul>
+                <li className='name'>Name: {user.name}</li>
+                <li>Username: {user.username}</li>
+                <li>Country: {user.nationality.toLowerCase()}</li>
+                <li className='friends'>Friends: {user.friends}</li>
+              </ul>
+              <hr />
+            </div>
+          ))}
+      </div>
+    </>
   )
 }
 
